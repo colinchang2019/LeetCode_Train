59685d3dd3cc9f5ca90ae6e83f1c13550433655a
Chesley <47071655+colinchang2019@users.noreply.github.com>
2020-04-04 17:22:12 +0800

    Create Leetcode42.py

---
+++ Leetcode42.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        h1 = 0
+        h2 = 0
+        for i in range(len(height)):
+            h1 = max(h1,height[i])
+            h2 = max(h2,height[-i-1])
+            ans = ans + h1 + h2 -height[i]
+        print(h1,h2)
+        return  ans - len(height)*h1
