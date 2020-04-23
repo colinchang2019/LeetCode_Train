6912a219ca993e10aeee8762d6c241fb5b2ca3e1
Chesley <47071655+colinchang2019@users.noreply.github.com>
2020-04-24 07:32:01 +0800

    Create 面试题51.py

---
+++ 面试题51.py
@@ -0,0 +1,29 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        tmp = [0 for _ in range(len(nums))]
+        return self.mergeSort(nums,tmp,0,len(nums)-1)
+
+    def mergeSort(self, nums, tmp, l, r):
+        if l >= r:
+            return 0
+        mid = (l + r) // 2
+        inv_count = self.mergeSort(nums, tmp, l, mid) + self.mergeSort(nums, tmp, mid + 1, r)
+        i, j, pos = l, mid + 1, l # 从左往右合并 填补到tmp中
+        while i <= mid and j <= r:
+            if nums[i] <= nums[j]:
+                tmp[pos] = nums[i]
+                i += 1
+                inv_count += (j - (mid + 1))
+            else:
+                tmp[pos] = nums[j]
+                j += 1
+            pos += 1
+        for k in range(i, mid + 1):
+            tmp[pos] = nums[k]
+            inv_count += (j - (mid + 1))
+            pos += 1
+        for k in range(j, r + 1):
+            tmp[pos] = nums[k]
+            pos += 1
+        nums[l:r+1] = tmp[l:r+1]
+        return inv_count
