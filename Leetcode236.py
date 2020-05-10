ed21482e23450a559d48acb9580a8a24fe21f6f2
Chesley <47071655+colinchang2019@users.noreply.github.com>
2020-05-10 08:50:57 +0800

    Create Leetcode236.py

---
+++ Leetcode236.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == None or root==p or root==q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right: 
+            return root
+        elif left:
+            return left  # 返回左子树查找结果
+        elif right:
+            return right  # 返回右子树查找结果
+        else:
+            return
