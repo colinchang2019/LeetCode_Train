b4dc2b15c2cb5563def6aea89e50ace747801124
Chesley <47071655+colinchang2019@users.noreply.github.com>
2020-05-27 13:42:26 +0800

    Update Leetcode974.py

--- Leetcode974.py
+++ Leetcode974.py
@@ -9,10 +9,10 @@ class Solution:
             res += re[modulus]
             re[modulus] += 1
         return res
-/*
+'''
  判断子数组的和能否被 KK 整除就等价于判断 (P[j] - P[i-1]) \bmod K == 0(P[j]−P[i−1])modK==0，根据 同余定理，只要 P[j] \bmod K == P[i-1] \bmod KP[j]modK==P[i−1]modK，就可以保证上面的等式成立。
 
 作者：LeetCode-Solution
 链接：https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/solution/he-ke-bei-k-zheng-chu-de-zi-shu-zu-by-leetcode-sol/
 来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。'''
