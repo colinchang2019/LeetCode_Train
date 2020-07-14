3a276fe9bdcfc547168d32fb0b35055cca85a200
Chesley <47071655+colinchang2019@users.noreply.github.com>
2020-07-14 09:22:16 +0800

    Create Leetcode120.py

---
+++ Leetcode120.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle
+        for i in range(1, len(triangle)):
+            for j in range(i+1):
+                if j == 0:
+                    dp[i][j] = dp[i-1][j] + triangle[i][j]
+                elif j == i:
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
+        tem = dp[len(triangle)-1][0]
+        for i in dp[len(triangle)-1]:
+            tem = min(tem, i)
+        return tem
