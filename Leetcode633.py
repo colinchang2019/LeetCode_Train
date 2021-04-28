4fa2c28a9e7ab677946fb9d2a9ac5dc80f64ff2e
Chesley <47071655+colinchang2019@users.noreply.github.com>
2021-04-28 09:53:38 +0800

    Create Leetcode633.py

---
+++ Leetcode633.py
@@ -0,0 +1,14 @@
+import math
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        j = int(math.sqrt(c))
+        i = 0
+        while i <= j:
+            total = i * i + j * j
+            if total > c:
+                j = j - 1
+            elif total < c:
+                i = i + 1
+            else:
+                return True
+        return False
